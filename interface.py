c40da7a9aea6eadc7f264cf79015782ce46a2e40
Shangwen <shangwen@Shangwens-MacBook-Pro.local>
2022-01-16 22:16:07 -0500

    add function to recieve test result

--- interface.py
+++ interface.py
@@ -9,4 +9,7 @@ def interface():
             keep_running = False
     return
    
+def accept_input(test_name):
+    entry = input("Enter the {} test result: ".format(test_name))
+    return int(entry)
 interface()
