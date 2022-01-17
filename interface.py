e61e999aa7b0f22234ea48c3400624ffc456f706
Shangwen <shangwen@Shangwens-MacBook-Pro.local>
2022-01-16 22:49:08 -0500

    add the HDL analysis option and call your HDL driver function

--- interface.py
+++ interface.py
@@ -3,15 +3,23 @@ def interface():
     print("Blodd Test Analysis")
     while keep_running:
         print("Options:")
+        print("1 - HDL")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice == 9:
             keep_running = False
+        elif choice == 1:
+            HDL_driver()
     return
    
 def accept_input(test_name):
     entry = input("Enter the {} test result: ".format(test_name))
     return int(entry)
+
+def print_result(test_name, test_value, test_class):
+    out_string = "The test value of {} for {} is {}".format(test_value, test_name, test_class)
+    print(out_string)
+    return out_string
 
 def check_HDL(HDL_value):
     if HDL_value >= 60:
@@ -24,6 +32,7 @@ def check_HDL(HDL_value):
 
 def HDL_driver():
     HDLvalue = accept_input("HDL")
-    classification = check_HDL
-    
+    classification = check_HDL(HDLvalue)
+    print_result("HDL", HDLvalue, classification)
+
 interface()
