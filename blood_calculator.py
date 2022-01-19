8a818f8536b984c6df5e84b9c53d94a269440cb8
Shangwen <shangwen@Shangwens-MacBook-Pro.local>
2022-01-19 15:57:23 -0500

    update

---
+++ blood_calculator.py
@@ -0,0 +1,39 @@
+def interface():
+    keep_running = True
+    print("Blodd Test Analysis")
+    while keep_running:
+        print("Options:")
+        print("1 - HDL")
+        print("9 - Quit")
+        choice = input("Enter your choice: ")
+        if choice == 9:
+            keep_running = False
+        elif choice == 1:
+            HDL_driver()
+    return
+   
+def accept_input(test_name):
+    entry = input("Enter the {} test result: ".format(test_name))
+    return int(entry)
+
+def print_result(test_name, test_value, test_class):
+    out_string = "The test value of {} for {} is {}".format(test_value, test_name, test_class)
+    print(out_string)
+    return out_string
+
+def check_HDL(HDL_value):
+    if HDL_value >= 60:
+        answer = "Normal"
+    elif 60 > HDL_value >= 40:
+        answer = "Borderline Low"
+    elif HDL_value < 40:
+        answer = "Low"
+    return answer
+
+def HDL_driver():
+    HDLvalue = accept_input("HDL")
+    classification = check_HDL(HDLvalue)
+    print_result("HDL", HDLvalue, classification)
+
+if __name__ == "main":
+    interface()
---
+++ database.py
@@ -0,0 +1,5 @@
+from blood_calculator import check_HDL
+
+HDL_value = 55
+classification = check_HDL(HDL_value)
+print("55 is {}".format(classification))
